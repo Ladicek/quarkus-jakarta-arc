a6c3ca47755f218c87e8365120033fc7950a86a6
Ladislav Thon <lthon@redhat.com>
2020-09-07 14:13:31 +0200

    sketch implementation of the new *Info interfaces on top of ArC

---
+++ independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/cdi/lite/ext/DisposerInfoImpl.java
@@ -0,0 +1,31 @@
+package io.quarkus.arc.processor.cdi.lite.ext;
+
+import cdi.lite.extension.model.beans.DisposerInfo;
+import cdi.lite.extension.model.declarations.MethodInfo;
+import cdi.lite.extension.model.declarations.ParameterInfo;
+import org.jboss.jandex.IndexView;
+
+class DisposerInfoImpl implements DisposerInfo {
+    private final org.jboss.jandex.IndexView jandexIndex;
+    private final AllAnnotationOverlays annotationOverlays;
+    private final io.quarkus.arc.processor.DisposerInfo arcDisposerInfo;
+
+    DisposerInfoImpl(IndexView jandexIndex, AllAnnotationOverlays annotationOverlays,
+            io.quarkus.arc.processor.DisposerInfo arcDisposerInfo) {
+        this.jandexIndex = jandexIndex;
+        this.annotationOverlays = annotationOverlays;
+        this.arcDisposerInfo = arcDisposerInfo;
+    }
+
+    @Override
+    public MethodInfo<?> disposerMethod() {
+        org.jboss.jandex.MethodInfo jandexMethod = arcDisposerInfo.getDisposerMethod();
+        return new MethodInfoImpl(jandexIndex, annotationOverlays, jandexMethod);
+    }
+
+    @Override
+    public ParameterInfo<?> disposedParameter() {
+        org.jboss.jandex.MethodParameterInfo jandexParameter = arcDisposerInfo.getDisposedParameter();
+        return new ParameterInfoImpl(jandexIndex, annotationOverlays, jandexParameter.method(), jandexParameter.position());
+    }
+}
---
+++ independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/cdi/lite/ext/ScopeInfoImpl.java
@@ -0,0 +1,29 @@
+package io.quarkus.arc.processor.cdi.lite.ext;
+
+import cdi.lite.extension.model.beans.ScopeInfo;
+import cdi.lite.extension.model.declarations.ClassInfo;
+import org.jboss.jandex.IndexView;
+
+class ScopeInfoImpl implements ScopeInfo {
+    private final org.jboss.jandex.IndexView jandexIndex;
+    private final AllAnnotationOverlays annotationOverlays;
+    private final io.quarkus.arc.processor.ScopeInfo arcScopeInfo;
+
+    ScopeInfoImpl(IndexView jandexIndex, AllAnnotationOverlays annotationOverlays,
+            io.quarkus.arc.processor.ScopeInfo arcScopeInfo) {
+        this.jandexIndex = jandexIndex;
+        this.annotationOverlays = annotationOverlays;
+        this.arcScopeInfo = arcScopeInfo;
+    }
+
+    @Override
+    public ClassInfo<?> annotation() {
+        org.jboss.jandex.ClassInfo jandexClass = jandexIndex.getClassByName(arcScopeInfo.getDotName());
+        return new ClassInfoImpl(jandexIndex, annotationOverlays, jandexClass);
+    }
+
+    @Override
+    public boolean isNormal() {
+        return arcScopeInfo.isNormal();
+    }
+}

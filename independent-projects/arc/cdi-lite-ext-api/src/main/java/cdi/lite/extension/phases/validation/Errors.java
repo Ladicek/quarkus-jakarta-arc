6b56016aad5f9a53ace6a4c7e1189002d888ddd3
Ladislav Thon <lthon@redhat.com>
2020-09-07 14:16:08 +0200

    sketch improvements to the validation API

--- independent-projects/arc/cdi-lite-ext-api/src/main/java/cdi/lite/extension/phases/validation/Errors.java
+++ independent-projects/arc/cdi-lite-ext-api/src/main/java/cdi/lite/extension/phases/validation/Errors.java
@@ -1,11 +1,33 @@
 package cdi.lite.extension.phases.validation;
 
 import cdi.lite.extension.model.AnnotationTarget;
+import cdi.lite.extension.model.beans.BeanInfo;
+import cdi.lite.extension.model.beans.ObserverInfo;
 
 public interface Errors {
+    /**
+     * Add a generic error that is not related to any particular element, or that information is not known.
+     */
     void add(String message);
 
+    /**
+     * Add an error which is related to given {@link AnnotationTarget} (which is most likely
+     * a {@link cdi.lite.extension.model.declarations.DeclarationInfo DeclarationInfo}).
+     */
     void add(String message, AnnotationTarget relatedTo);
 
+    /**
+     * Add an error which is related to given {@link BeanInfo}.
+     */
+    void add(String message, BeanInfo<?> relatedTo);
+
+    /**
+     * Add an error which is related to given {@link ObserverInfo}.
+     */
+    void add(String message, ObserverInfo<?> relatedTo);
+
+    /**
+     * Add a generic error that is represented by an exception.
+     */
     void add(Exception exception);
 }

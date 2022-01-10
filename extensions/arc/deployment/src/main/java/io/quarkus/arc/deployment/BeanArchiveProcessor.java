Ladicek/quarkus-fork

prototype implementation of the AppArchiveBuilder part of @Discovery
This commit also changes the implementations of extensions lookup.
Since the `@Discovery` phase must run before bean archive exists,
we can no longer search for extensions in the bean archive.

We planned to use `ServiceLoader` for this, and now is the time
to actually implement it. The rest of the extension infrastructure
is still on Jandex, which leads to some really silly code. That
should later be refactored.

extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/BeanArchiveProcessor.java
@@ -22,6 +22,7 @@ import io.quarkus.arc.processor.BeanArchives;
 import io.quarkus.arc.processor.BeanDefiningAnnotation;
 import io.quarkus.arc.processor.BeanDeployment;
 import io.quarkus.arc.processor.DotNames;
+import io.quarkus.arc.processor.cdi.lite.ext.CdiLiteExtDiscoveryProcessor;
 import io.quarkus.arc.runtime.LifecycleEventRunner;
 import io.quarkus.bootstrap.model.AppArtifactKey;
 import io.quarkus.deployment.ApplicationArchive;
@@ -64,6 +65,10 @@ public class BeanArchiveProcessor {
         }
         // NOTE: the types added directly must always declare a scope annotation otherwise they will be ignored during bean discovery
         additionalBeans.add(LifecycleEventRunner.class.getName());
+
+        Set<String> additionalBeansFromCdiLiteExtensions = new HashSet<>();
+        new CdiLiteExtDiscoveryProcessor(applicationIndex, additionalBeansFromCdiLiteExtensions).run();
+        additionalBeans.addAll(additionalBeansFromCdiLiteExtensions);
 
         // Build the index for additional beans and generated bean classes
         Set<DotName> additionalIndex = new HashSet<>();

Ladicek/quarkus-jakarta-arc

extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/BeanArchiveProcessor.java
@@ -21,6 +21,7 @@ import io.quarkus.arc.processor.BeanArchives;
 import io.quarkus.arc.processor.BeanDefiningAnnotation;
 import io.quarkus.arc.processor.BeanDeployment;
 import io.quarkus.arc.processor.DotNames;
+import io.quarkus.arc.processor.cdi.lite.ext.CdiLiteExtDiscoveryProcessor;
 import io.quarkus.deployment.ApplicationArchive;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
@@ -54,6 +55,10 @@ public class BeanArchiveProcessor {
         for (AdditionalBeanBuildItem i : additionalBeans) {
             additionalBeanClasses.addAll(i.getBeanClasses());
         }
+
+        Set<String> additionalBeansFromCdiLiteExtensions = new HashSet<>();
+        new CdiLiteExtDiscoveryProcessor(applicationIndex, additionalBeansFromCdiLiteExtensions).run();
+        additionalBeanClasses.addAll(additionalBeansFromCdiLiteExtensions);
 
         // Build the index for additional beans and generated bean classes
         Set<DotName> additionalIndex = new HashSet<>();
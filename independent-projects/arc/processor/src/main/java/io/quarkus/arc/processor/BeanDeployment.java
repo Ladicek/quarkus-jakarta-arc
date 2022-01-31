Ladicek/quarkus-fork

update the Quarkus implementation of Build Compatible Extensions
This commit synchronizes the proposed Build Compatible Extensions API
with the StillDI project, and implements the required changes.

That includes some additions to the ArC extension API. Specifically,
registering additional stereotype annotations is now possible, and
array-typed parameters can now be used when creating synthetic beans.

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanDeployment.java
@@ -173,8 +173,13 @@ public class BeanDeployment {
         repeatingInterceptorBindingAnnotations = findContainerAnnotations(interceptorBindings, this.beanArchiveIndex);
         buildContextPut(Key.INTERCEPTOR_BINDINGS.asString(), Collections.unmodifiableMap(interceptorBindings));
 
+        Set<DotName> stereotypeRegistrars = new HashSet<>();
+        for (StereotypeRegistrar stereotypeRegistrar : builder.stereotypeRegistrars) {
+            stereotypeRegistrars.addAll(stereotypeRegistrar.registerAdditionalStereotypes());
+        }
+
         this.stereotypes = findStereotypes(this.beanArchiveIndex, interceptorBindings, beanDefiningAnnotations, customContexts,
-                builder.additionalStereotypes, annotationStore);
+                builder.additionalStereotypes, stereotypeRegistrars, annotationStore);
         buildContextPut(Key.STEREOTYPES.asString(), Collections.unmodifiableMap(stereotypes));
 
         this.transitiveInterceptorBindings = findTransitiveInterceptorBindigs(interceptorBindings.keySet(),
@@ -633,10 +638,14 @@ public class BeanDeployment {
     private Map<DotName, StereotypeInfo> findStereotypes(IndexView index, Map<DotName, ClassInfo> interceptorBindings,
             Collection<BeanDefiningAnnotation> additionalBeanDefiningAnnotations,
             Map<ScopeInfo, Function<MethodCreator, ResultHandle>> customContexts,
-            Map<DotName, Collection<AnnotationInstance>> additionalStereotypes, AnnotationStore annotationStore) {
+            Map<DotName, Collection<AnnotationInstance>> additionalStereotypes, Set<DotName> stereotypeRegistrars,
+            AnnotationStore annotationStore) {
 
         Map<DotName, StereotypeInfo> stereotypes = new HashMap<>();
         final List<AnnotationInstance> stereotypeAnnotations = new ArrayList<>(index.getAnnotations(DotNames.STEREOTYPE));
+        for (DotName stereotypeRegistrar : stereotypeRegistrars) {
+            stereotypeAnnotations.addAll(index.getAnnotations(stereotypeRegistrar));
+        }
         for (final Collection<AnnotationInstance> annotations : additionalStereotypes.values()) {
             stereotypeAnnotations.addAll(annotations);
         }
@@ -665,6 +674,9 @@ public class BeanDeployment {
                         }
                         isNamed = true;
                     } else if (DotNames.PRIORITY.equals(annotation.name())) {
+                        alternativePriority = annotation.value().asInt();
+                    } else if (DotNames.ALTERNATIVE_PRIORITY.equals(annotation.name())) {
+                        isAlternative = true;
                         alternativePriority = annotation.value().asInt();
                     } else {
                         final ScopeInfo scope = getScope(annotation.name(), customContexts);

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanGenerator.java
@@ -190,20 +190,56 @@ public class BeanGenerator extends AbstractGenerator {
             for (Entry<String, Object> entry : bean.getParams().entrySet()) {
                 ResultHandle valHandle = null;
                 if (entry.getValue() instanceof String) {
-                    valHandle = constructor.load(entry.getValue().toString());
+                    valHandle = constructor.load((String) entry.getValue());
+                } else if (entry.getValue() instanceof String[]) {
+                    String[] array = (String[]) entry.getValue();
+                    valHandle = constructor.newArray(String.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Integer) {
                     valHandle = constructor.newInstance(MethodDescriptor.ofConstructor(Integer.class, int.class),
                             constructor.load(((Integer) entry.getValue()).intValue()));
+                } else if (entry.getValue() instanceof int[]) {
+                    int[] array = (int[]) entry.getValue();
+                    valHandle = constructor.newArray(int.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Long) {
                     valHandle = constructor.newInstance(MethodDescriptor.ofConstructor(Long.class, long.class),
                             constructor.load(((Long) entry.getValue()).longValue()));
+                } else if (entry.getValue() instanceof long[]) {
+                    long[] array = (long[]) entry.getValue();
+                    valHandle = constructor.newArray(double.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Double) {
                     valHandle = constructor.newInstance(MethodDescriptor.ofConstructor(Double.class, double.class),
                             constructor.load(((Double) entry.getValue()).doubleValue()));
+                } else if (entry.getValue() instanceof double[]) {
+                    double[] array = (double[]) entry.getValue();
+                    valHandle = constructor.newArray(double.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Class) {
                     valHandle = constructor.loadClass((Class<?>) entry.getValue());
+                } else if (entry.getValue() instanceof Class[]) {
+                    Class<?>[] array = (Class<?>[]) entry.getValue();
+                    valHandle = constructor.newArray(Class.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.loadClass(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Boolean) {
                     valHandle = constructor.load((Boolean) entry.getValue());
+                } else if (entry.getValue() instanceof boolean[]) {
+                    boolean[] array = (boolean[]) entry.getValue();
+                    valHandle = constructor.newArray(boolean.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 }
                 // TODO other param types
                 constructor.invokeInterfaceMethod(MethodDescriptors.MAP_PUT, paramsHandle, constructor.load(entry.getKey()),

Ladicek/quarkus-jakarta-arc

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanDeployment.java
@@ -178,8 +178,13 @@ public class BeanDeployment {
         repeatingInterceptorBindingAnnotations = findContainerAnnotations(interceptorBindings, this.beanArchiveIndex);
         buildContextPut(Key.INTERCEPTOR_BINDINGS.asString(), Collections.unmodifiableMap(interceptorBindings));
 
+        Set<DotName> additionalStereotypes = new HashSet<>();
+        for (StereotypeRegistrar stereotypeRegistrar : builder.stereotypeRegistrars) {
+            additionalStereotypes.addAll(stereotypeRegistrar.getAdditionalStereotypes());
+        }
+
         this.stereotypes = findStereotypes(this.beanArchiveIndex, interceptorBindings, beanDefiningAnnotations, customContexts,
-                builder.additionalStereotypes, annotationStore);
+                builder.additionalStereotypes, additionalStereotypes, annotationStore);
         buildContextPut(Key.STEREOTYPES.asString(), Collections.unmodifiableMap(stereotypes));
 
         this.transitiveInterceptorBindings = findTransitiveInterceptorBindings(interceptorBindings.keySet(),
@@ -726,19 +731,23 @@ public class BeanDeployment {
     private Map<DotName, StereotypeInfo> findStereotypes(IndexView index, Map<DotName, ClassInfo> interceptorBindings,
             Collection<BeanDefiningAnnotation> additionalBeanDefiningAnnotations,
             Map<ScopeInfo, Function<MethodCreator, ResultHandle>> customContexts,
-            Map<DotName, Collection<AnnotationInstance>> additionalStereotypes, AnnotationStore annotationStore) {
+            Map<DotName, Collection<AnnotationInstance>> legacyAdditionalStereotypes, Set<DotName> additionalStereotypes,
+            AnnotationStore annotationStore) {
 
         Map<DotName, StereotypeInfo> stereotypes = new HashMap<>();
-        final List<AnnotationInstance> stereotypeAnnotations = new ArrayList<>(index.getAnnotations(DotNames.STEREOTYPE));
-        for (final Collection<AnnotationInstance> annotations : additionalStereotypes.values()) {
-            stereotypeAnnotations.addAll(annotations);
-        }
+
         Set<DotName> stereotypeNames = new HashSet<>();
-        for (AnnotationInstance stereotype : stereotypeAnnotations) {
-            stereotypeNames.add(stereotype.target().asClass().name());
+        for (AnnotationInstance annotation : index.getAnnotations(DotNames.STEREOTYPE)) {
+            stereotypeNames.add(annotation.target().asClass().name());
         }
-        for (AnnotationInstance stereotype : stereotypeAnnotations) {
-            final DotName stereotypeName = stereotype.target().asClass().name();
+        stereotypeNames.addAll(additionalStereotypes);
+        for (Collection<AnnotationInstance> annotations : legacyAdditionalStereotypes.values()) {
+            for (AnnotationInstance annotation : annotations) {
+                stereotypeNames.add(annotation.target().asClass().name());
+            }
+        }
+
+        for (DotName stereotypeName : stereotypeNames) {
             ClassInfo stereotypeClass = getClassByName(index, stereotypeName);
             if (stereotypeClass != null && !isExcluded(stereotypeClass)) {
 
@@ -766,6 +775,9 @@ public class BeanDeployment {
                         isNamed = true;
                     } else if (DotNames.PRIORITY.equals(annotation.name())) {
                         alternativePriority = annotation.value().asInt();
+                    } else if (DotNames.ALTERNATIVE_PRIORITY.equals(annotation.name())) {
+                        isAlternative = true;
+                        alternativePriority = annotation.value().asInt();
                     } else {
                         final ScopeInfo scope = getScope(annotation.name(), customContexts);
                         if (scope != null) {
@@ -773,7 +785,8 @@ public class BeanDeployment {
                         }
                     }
                 }
-                boolean isAdditionalStereotypeBuildItem = additionalStereotypes.containsKey(stereotypeName);
+                boolean isAdditionalStereotypeBuildItem = legacyAdditionalStereotypes.containsKey(stereotypeName)
+                        || additionalStereotypes.contains(stereotypeName);
                 final ScopeInfo scope = getValidScope(scopes, stereotypeClass);
                 boolean isInherited = stereotypeClass.classAnnotation(DotNames.INHERITED) != null;
                 stereotypes.put(stereotypeName, new StereotypeInfo(scope, bindings, isAlternative, alternativePriority,

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanGenerator.java
@@ -194,20 +194,56 @@ public class BeanGenerator extends AbstractGenerator {
             for (Entry<String, Object> entry : bean.getParams().entrySet()) {
                 ResultHandle valHandle = null;
                 if (entry.getValue() instanceof String) {
-                    valHandle = constructor.load(entry.getValue().toString());
+                    valHandle = constructor.load((String) entry.getValue());
+                } else if (entry.getValue() instanceof String[]) {
+                    String[] array = (String[]) entry.getValue();
+                    valHandle = constructor.newArray(String.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Integer) {
                     valHandle = constructor.newInstance(MethodDescriptor.ofConstructor(Integer.class, int.class),
                             constructor.load(((Integer) entry.getValue()).intValue()));
+                } else if (entry.getValue() instanceof int[]) {
+                    int[] array = (int[]) entry.getValue();
+                    valHandle = constructor.newArray(int.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Long) {
                     valHandle = constructor.newInstance(MethodDescriptor.ofConstructor(Long.class, long.class),
                             constructor.load(((Long) entry.getValue()).longValue()));
+                } else if (entry.getValue() instanceof long[]) {
+                    long[] array = (long[]) entry.getValue();
+                    valHandle = constructor.newArray(double.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Double) {
                     valHandle = constructor.newInstance(MethodDescriptor.ofConstructor(Double.class, double.class),
                             constructor.load(((Double) entry.getValue()).doubleValue()));
+                } else if (entry.getValue() instanceof double[]) {
+                    double[] array = (double[]) entry.getValue();
+                    valHandle = constructor.newArray(double.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Class) {
                     valHandle = constructor.loadClassFromTCCL((Class<?>) entry.getValue());
+                } else if (entry.getValue() instanceof Class[]) {
+                    Class<?>[] array = (Class<?>[]) entry.getValue();
+                    valHandle = constructor.newArray(Class.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.loadClassFromTCCL(array[i]));
+                    }
                 } else if (entry.getValue() instanceof Boolean) {
                     valHandle = constructor.load((Boolean) entry.getValue());
+                } else if (entry.getValue() instanceof boolean[]) {
+                    boolean[] array = (boolean[]) entry.getValue();
+                    valHandle = constructor.newArray(boolean.class, array.length);
+                    for (int i = 0; i < array.length; i++) {
+                        constructor.writeArrayValue(valHandle, i, constructor.load(array[i]));
+                    }
                 }
                 // TODO other param types
                 constructor.invokeInterfaceMethod(MethodDescriptors.MAP_PUT, paramsHandle, constructor.load(entry.getKey()),